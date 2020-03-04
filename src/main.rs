67bb52f5477200992b5a07a9ed70b361b245044d
Michael Zijlstra <mzijlstra@gmail.com>
2020-03-04 11:54:02 +0100

    Sets up Left to Right Flight
    Plane now flies left to right, and moves all 4 directions.

--- src/main.rs
+++ src/main.rs
@@ -1,115 +1,302 @@
 extern crate sdl2;
 
-use std::path::Path;
 use sdl2::event::Event;
-use sdl2::image::LoadTexture;
 use sdl2::image::InitFlag;
+use sdl2::image::LoadTexture;
 use sdl2::keyboard::Keycode;
-use sdl2::rect::Rect;
 use sdl2::rect::Point;
+use sdl2::rect::Rect;
+use std::path::Path;
 use std::time::Duration;
 
+struct Player {
+    source: Rect,
+    dest: Rect,
+    up: bool,
+    down: bool,
+    left: bool,
+    right: bool,
+    up_count: u32,
+    down_count: u32,
+    flame: i32,
+}
+
+impl Default for Player {
+    fn default() -> Player {
+        Player {
+            source: Rect::new(0,0,0,0),
+            dest: Rect::new(0,0,0,0),
+            up: false,
+            down: false,
+            left: false,
+            right: false,        
+            up_count: 0,
+            down_count: 0,
+            flame: 0,
+        }
+    }
+}
+
 fn main() -> Result<(), String> {
+    let window_size: (i32, i32) = (480, 300);
     let sdl_context = sdl2::init()?;
     let video_subsystem = sdl_context.video()?;
     let _image_context = sdl2::image::init(InitFlag::PNG)?;
 
-    let window = video_subsystem.window("SDL2", 640, 480)
+    let window = video_subsystem
+        .window("SDL2", window_size.0 as u32, window_size.1 as u32)
         .position_centered()
+        //.fullscreen()
         .build()
         .map_err(|e| e.to_string())?;
 
-    let mut canvas = window.into_canvas()
+
+    let mut canvas = window
+        .into_canvas()
         .accelerated()
         .build()
         .map_err(|e| e.to_string())?;
 
     let texture_creator = canvas.texture_creator();
-    canvas.set_draw_color(sdl2::pixels::Color::RGBA(0,0,0,255));
+    canvas.set_draw_color(sdl2::pixels::Color::RGBA(0, 0, 0, 255));
 
-    // animation sheet and extras are available from
-    // https://opengameart.org/content/a-platformer-in-the-forest
-    let texture = texture_creator.load_texture(Path::new("assets/characters.png"))?;
+    // sprites and background form: https://opengameart.org/content/space-ship-shooter-pixel-art-assets
+    let ship_texture = texture_creator.load_texture(Path::new("assets/ship.png"))?;
+    let bg_texture = texture_creator.load_texture(Path::new("assets/desert-background.png"))?;
 
-    let frames_per_anim = 4;
-    let sprite_tile_size = (32,32);
+    // background
+    let source_bg = Rect::new(0, 0, 272, 256);
+    let mut dest_bg_array: [Rect; 6] = [
+        Rect::new(0, 0, 272, 256),
+        Rect::new(272, 0, 272, 256),
+        Rect::new(544, 0, 272, 256),
+        Rect::new(0, 256, 272, 256),
+        Rect::new(272, 256, 272, 256),
+        Rect::new(544, 256, 272, 256),
+    ];
 
-    // King - walk animation
-    let mut source_rect_1 = Rect::new(0, 32, sprite_tile_size.0, sprite_tile_size.0);
-    let mut dest_rect_1 = Rect::new(0, 32, sprite_tile_size.0*4, sprite_tile_size.0*4);
-    dest_rect_1.center_on(Point::new(320,240));
+    // ship related
+    let frames_per_anim = 2;
+    let sprite_tile_size = (24, 16);
+
+    let mut player = Player {
+        source: Rect::new(24, 32, sprite_tile_size.0, sprite_tile_size.1),
+        dest: Rect::new(0, 0, sprite_tile_size.0, sprite_tile_size.1),
+        ..Default::default()
+    };
+    player.dest.center_on(Point::new(window_size.0 / 4, window_size.1 / 2));
 
     let mut timer = sdl_context.timer()?;
     let mut event_pump = sdl_context.event_pump()?;
 
     let mut running = true;
-    let mut going_left = false;
-    let mut frame = 0;
-    let mut moving = false;
+    let mut frame_count = 0;
+    let mut prev_second = 0;
     while running {
         let start_tick = timer.ticks();
+        frame_count += 1;
 
         for event in event_pump.poll_iter() {
             match event {
-                Event::KeyDown {keycode: Some(Keycode::Left), ..} => {
-                    moving = true;
-                    going_left = true;
+                Event::KeyDown {
+                    keycode: Some(Keycode::Up),
+                    ..
+                } | Event::KeyDown {
+                    keycode: Some(Keycode::W),
+                    ..
+                }=> {
+                    if !player.up {
+                        player.up = true;
+                        player.down = false;
+                        player.up_count = 0;    
+                    }
                 }
-                Event::KeyDown {keycode: Some(Keycode::Right), ..} => {
-                    moving = true;
-                    going_left = false;
+                Event::KeyUp {
+                    keycode: Some(Keycode::Up),
+                    ..
+                } | Event::KeyUp {
+                    keycode: Some(Keycode::W),
+                    ..
+                } => {
+                    player.up = false;
                 }
-                Event::KeyUp {keycode: Some(Keycode::Left), ..} | Event::KeyUp {keycode: Some(Keycode::Right), ..} => {
-                    moving = false;
+                Event::KeyDown {
+                    keycode: Some(Keycode::Down),
+                    ..
+                } | Event::KeyDown {
+                    keycode: Some(Keycode::S),
+                    ..
+                } => {
+                    if !player.down {
+                        player.down = true;
+                        player.up = false;
+                        player.down_count = 0;    
+                    }
                 }
-                Event::Quit {..} | Event::KeyDown {keycode: Some(Keycode::Escape), ..} => {
+                Event::KeyUp {
+                    keycode: Some(Keycode::Down),
+                    ..
+                } | Event::KeyUp {
+                    keycode: Some(Keycode::S),
+                    ..
+                } => {
+                    player.down = false;
+                }
+                Event::KeyDown {
+                    keycode: Some(Keycode::Left),
+                    ..
+                } | Event::KeyDown {
+                    keycode: Some(Keycode::A),
+                    ..
+                } => {
+                    player.right = false;
+                    player.left = true;
+                }
+                Event::KeyUp {
+                    keycode: Some(Keycode::Left),
+                    ..
+                } | Event::KeyUp {
+                    keycode: Some(Keycode::A),
+                    ..
+                } => {
+                    player.left = false;
+                }
+                Event::KeyDown {
+                    keycode: Some(Keycode::Right),
+                    ..
+                } | Event::KeyDown {
+                    keycode: Some(Keycode::D),
+                    ..
+                } => {
+                    player.left = false;
+                    player.right = true;
+                }
+                Event::KeyUp {
+                    keycode: Some(Keycode::Right),
+                    ..
+                } | Event::KeyUp {
+                    keycode: Some(Keycode::D),
+                    ..
+                } => {
+                    player.right = false;
+                }
+                Event::Quit { .. }
+                | Event::KeyDown {
+                    keycode: Some(Keycode::Escape),
+                    ..
+                } => {
                     running = false;
-                },
+                }
                 _ => {}
             }
         }
 
-        let mut new_x = dest_rect_1.x();
-        if moving {
-            let delta;
-            frame = (frame + 1) % frames_per_anim;
-            if going_left {
-                delta = -6
-            } else {
-                delta = 6;
+        // move background parts
+        for dest_bg in &mut dest_bg_array {
+            let mut bg_x = dest_bg.x();
+            if bg_x - 1 < -272 {
+                bg_x = 544;
             }
-            if new_x + delta < -128 {
-                new_x = 608;
-            } else if new_x + delta > 608 {
-                new_x = -128;
-            } else {
-                new_x = new_x + delta;
-            }
-        } else {
-            frame = 0;
+            dest_bg.set_x(bg_x - 1);
         }
 
-        source_rect_1.set_x(32 * frame);
-        dest_rect_1.set_x( new_x );
+        // move ship
+        let mut ship_x = player.dest.x();
+        let mut ship_y = player.dest.y();
+        let mut ship_tilt = 32;
+        if player.up {
+            // move ship
+            let delta_y = -1;
+            if ship_y + delta_y >= -8 {
+                ship_y = ship_y + delta_y;
+            }
+            // tilt ship
+            player.up_count += 1;
+            if player.up_count > 15 {
+                ship_tilt = 0;
+            } else {
+                ship_tilt = 16;
+            }
+        } else if player.down {
+            // move ship
+            let delta_y = 1;
+            if ship_y + delta_y < 8 + window_size.1 - sprite_tile_size.1 as i32 {
+                ship_y = ship_y + delta_y;
+            }
+            // tilt ship
+            player.down_count += 1;
+            if player.down_count > 15 {
+                ship_tilt = 64;
+            } else {
+                ship_tilt = 48;
+            }
+        }
+        
+        if player.right {
+            // animate exhaust flame
+            if frame_count % 5 == 0 {
+                player.flame = (player.flame + 1) % frames_per_anim;
+            }
+            let delta_x = 1;
+            if ship_x + delta_x < window_size.0 - sprite_tile_size.0 as i32 {
+                ship_x += delta_x;
+            }
+        } else if player.left {
+            let delta_x = -1;
+            if ship_x + delta_x > 0 {
+                ship_x += delta_x;
+            }
+            player.flame = 3;
+        } else {
+            player.flame = 2;
+        }
+        player.source.set_y(ship_tilt);
+        player.source.set_x(24 * player.flame);
+        player.dest.set_x(ship_x);
+        player.dest.set_y(ship_y);
 
+        // draw on screen
         canvas.clear();
-        // copy the frame to the canvas
-        canvas.copy_ex(&texture, Some(source_rect_1), Some(dest_rect_1), 0.0, None, going_left, false)?;
+        for dest_bg in &dest_bg_array {
+            canvas.copy_ex(
+                &bg_texture,
+                Some(source_bg),
+                Some(*dest_bg),
+                0.0,
+                None,
+                false,
+                false,
+            )?;
+        }
+        canvas.copy_ex(
+            &ship_texture,
+            Some(player.source),
+            Some(player.dest),
+            0.0,
+            None,
+            false,
+            false,
+        )?;
         canvas.present();
 
         let stop_tick = timer.ticks();
+        if stop_tick / 1000 > prev_second {
+            //println!("frames: {}", frame_count);
+            frame_count = 0;
+            prev_second += 1;
+        }
+
         let frame_time = stop_tick - start_tick;
 
-        if frame_time < 100 {
-            let sleep_time = (100 - frame_time) as u64;
+        if frame_time < 10 {
+            let sleep_time = (10 - frame_time) as u64;
             if sleep_time > 1 {
-                std::thread::sleep(Duration::from_millis(sleep_time));    
+                std::thread::sleep(Duration::from_millis(sleep_time));
             }
         } else {
-            println!("BIG frame time: {}", frame_time);
+            //println!("BIG frame time: {}", frame_time);
         }
     }
 
     Ok(())
 }
-
